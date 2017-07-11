2a9a39f4668b5e2b306bf068c3690e2ac1715e59
roysez <serhii.balukh.pz.2016@lpnu.ua>
2017-07-11 23:34:55 +0300

    expired search

--- src/main/java/com/market/domain/core/ad/AdRepository.java
+++ src/main/java/com/market/domain/core/ad/AdRepository.java
@@ -8,8 +8,12 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 
 public interface AdRepository extends PagingAndSortingRepository<Ad, Long> {
-
+    
     @RestResource(rel = "published",path = "published")
     @Query("select ad from Ad ad where ad.status = 'PUBLISHED'")
     Page<Ad> findPublished(Pageable pageable);
+
+    @RestResource(rel = "expired",path = "expired")
+    @Query("select ad from Ad ad where ad.status = 'EXPIRED'")
+    Page<Ad> findExpired(Pageable pageable);
 }
